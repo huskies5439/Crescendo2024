fcd646e91fa3e257de0dcf29ea04b0955395940c
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2024-02-10 14:53:05 -0500

    id du gobeur

--- src/main/java/frc/robot/subsystems/Gobeur.java
+++ src/main/java/frc/robot/subsystems/Gobeur.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Gobeur extends SubsystemBase {
 
-  private final TalonFX moteur = new TalonFX(5);
+  private final TalonFX moteur = new TalonFX(6);
 
   /** Creates a new Gobeur. */
   public Gobeur() {
